614a02bf4a527c81d886e811fabb5101190b0928
brooksnithiwat <167616396+brooksnithiwat@users.noreply.github.com>
2024-05-15 07:27:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage
+from urllib.parse import quote as url_quote  # แทนที่การนำเข้า url_quote จาก werkzeug
 
 app = Flask(__name__)
 
